7f9519514fd801bb368ea1a0a420ee43a4aaa72b
Yuichiro NAKAGAWA <ii.hsif.drows@gmail.com>
2014-04-10 22:41:32 +0900

    init

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+Bundler.require(:default, Rails.env)
+
+module DNATest
+  class Application < Rails::Application
+    config.i18n.default_locale = :ja
+    config.generators do |g|
+      g.test_framework nil
+    end
+
+    config.sass.preferred_syntax = :sass
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DNATest::Application.config.secret_key_base = 'fe0916b74a22c58b546e0a3b3d53455863bd25d7b6a261ab777399d92d6da025898cff4448e976197518892802b2b6b4340fc9c0bddfc3486b0b6f92ad8357bc'
