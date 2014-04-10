0aabf78ee565f8a748c7c54699cf8f19a8c99132
Yuichiro NAKAGAWA <ii.hsif.drows@gmail.com>
2014-04-11 03:29:44 +0900

    Bullet stop notifying to growl

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,6 @@ DNATest::Application.configure do
     Bullet.alert = true
     Bullet.bullet_logger = true
     Bullet.console = true
-    Bullet.growl = true
     Bullet.rails_logger = true
     Bullet.add_footer = true
   end
